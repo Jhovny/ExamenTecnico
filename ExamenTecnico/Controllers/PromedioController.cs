1cb9283503940ea48fc8407c183e8ac366294e46
Jhovny <jfloresninaco@gmail.com>
2020-10-09 19:00:42 -0500

    Todo

---
+++ ExamenTecnico/Controllers/PromedioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamenTecnico.Controllers
+{
+    public class PromedioController : Controller
+    {
+        // GET: Promedio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
